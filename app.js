5fd22a90af4790d0f08bddd8ffdc1194c7c9af54
Alicia Poindexter <alcampbel@gmail.com>
2016-02-11 18:03:08 -0800

    add stormpath

--- app.js
+++ app.js
@@ -11,6 +11,17 @@ var logger = require('morgan');
 
 var app = express();
 
+var stormpath = require('express-stormpath');
+// var apiKey = new stormpath.ApiKey(
+//   process.env.STORMPATH_CLIENT_APIKEY_ID,
+//   process.env.STORMPATH_CLIENT_APIKEY_SECRET
+// );
+// var client = new stormpath.Client({ apiKey: apiKey });
+// var applicationHref = process.env.STORMPATH_APPLICATION_HREF;
+// client.getApplication(applicationHref, function(err, application) {
+//   console.log('Application:', application);
+// });
+
 var mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/[ENTER_PROJECT_NAME_HERE]')
@@ -23,6 +34,18 @@ app.use(logger('dev'));
 app.use(bodyParser.urlencoded( {extended: true} ));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(stormpath.init(app, {
+  client: {
+    apiKey: {
+      id: process.env.STORMPATH_CLIENT_APIKEY_ID,
+      secret: process.env.STORMPATH_CLIENT_APIKEY_SECRET,
+    }
+  },
+  application: {
+    href: process.env.STORMPATH_APPLICATION_HREF
+  },
+  website: true
+}));
 
 app.use('/', require('./routes/index'));
 
@@ -31,6 +54,8 @@ app.get(function(req, res) {
   res.status(404).render('404');
 });
 
-app.listen(PORT, function(){
-  console.log('Listening on port ', PORT);
+app.on('stormpath.ready', function() {
+  app.listen(PORT, function() {
+    console.log('Listening on port ', PORT);
+  });
 });
